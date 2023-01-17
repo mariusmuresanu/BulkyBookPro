9f78679e81c97e0ac8e65aa1f012e96ec8be3000
muresanu marius <mariusmuresanu@yahoo.com>
2023-01-17 12:45:56 +0200

    Added connection string in appsetinggs.json. Added DbContext with Db set for Category table in DB

---
+++ BulkyBookWebPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWebPro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWebPro.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
