7ffada47eec36d5683ba289eb07f32418b0d0c2a
muresanu marius <mariusmuresanu@yahoo.com>
2023-01-18 14:19:10 +0200

    Added DataAnnotation to rename model on client side

--- BulkyBookWebPro/Models/Category.cs
+++ BulkyBookWebPro/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
 
 namespace BulkyBookWebPro.Models
@@ -9,6 +10,7 @@ namespace BulkyBookWebPro.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
