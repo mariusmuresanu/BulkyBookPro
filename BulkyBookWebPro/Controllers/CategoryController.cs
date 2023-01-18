6a1ac4774da9c827e3ce41791afe005f87241de4
muresanu marius <mariusmuresanu@yahoo.com>
2023-01-18 13:00:25 +0200

    Add post method in CreateConreoller to populate categories table in db

--- BulkyBookWebPro/Controllers/CategoryController.cs
+++ BulkyBookWebPro/Controllers/CategoryController.cs
@@ -21,5 +21,14 @@ namespace BulkyBookWebPro.Controllers
         {
             return View();
         }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");   
+        }
     }
 }
