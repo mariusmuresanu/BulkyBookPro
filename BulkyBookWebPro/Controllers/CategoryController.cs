d1342077a7e5d5aad5d47deb647625af431ae6d0
muresanu marius <mariusmuresanu@yahoo.com>
2023-01-23 10:16:40 +0200

    Added generic Repository

--- BulkyBookWebPro/Controllers/CategoryController.cs
+++ BulkyBookWebPro/Controllers/CategoryController.cs
@@ -48,16 +48,16 @@ namespace BulkyBookWebPro.Controllers
             {
                 return NotFound();
             }
-            var categoryFromDb = _db.Categories.Find(id);
-            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Name == "id");
             //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
 
-            if (categoryFromDb == null)
+            if (categoryFromDbFirst == null)
             {
                 return NotFound();
             }
 
-            return View(categoryFromDb);
+            return View(categoryFromDbFirst);
         }
         //POST
         [HttpPost]
