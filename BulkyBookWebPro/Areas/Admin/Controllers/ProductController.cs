e8e2a130a3d8e423f576d07a750c37071a8810c5
muresanu marius <mariusmuresanu@yahoo.com>
2023-01-30 13:20:56 +0200

    Added File Upload to Product

--- BulkyBookWebPro/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWebPro/Areas/Admin/Controllers/ProductController.cs
@@ -60,12 +60,12 @@ public class ProductController : Controller
     //POST
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Upsert(CoverType obj)
+    public IActionResult Upsert(ProductVM obj, IFormFile file)
     {
 
         if (ModelState.IsValid)
         {
-            _unitOfWork.CoverType.Update(obj);
+            //_unitOfWork.CoverType.Update(obj);
             _unitOfWork.Save();
             TempData["success"] = "CoverType edited successfully";
             return RedirectToAction("Index");
